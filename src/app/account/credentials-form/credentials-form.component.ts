2a2ab409639a7788300a2080b4ebf71cf19f4306
Karsten Larson <karsten.larson.1@gmail.com>
2025-04-14 09:12:39 -0500

    Initial Commit

---
+++ src/app/account/credentials-form/credentials-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-credentials-form',
+  imports: [],
+  templateUrl: './credentials-form.component.html',
+  styleUrl: './credentials-form.component.css'
+})
+export class CredentialsFormComponent {
+
+}
---
+++ src/app/shared/types/CartItem.ts
@@ -0,0 +1,19 @@
+class CartItem {
+  private _quantity: number = 0;
+
+  constructor(quantity: number, public product: Product) {
+    this.quantity = quantity;
+  }
+
+  get quantity(): number {
+    return this._quantity;
+  }
+
+  set quantity(value: number) {
+    if (value < 1) {
+      throw new Error('Quantity cannot be zero or negative');
+    }
+
+    this._quantity = value;
+  }
+}
