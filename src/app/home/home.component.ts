c14c988736a323a470508b1d6efdef589ff70fdc
Karsten Larson <karsten.larson.1@gmail.com>
2025-04-19 23:56:43 -0500

    Home page and style changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ProductCardComponent } from '../product/product-card/product-card.component';
+import { ProductService } from '../product.service';
+import { Product } from '../shared/types/Product';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink, ProductCardComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  displayProducts!: Product[];
+  productService = inject(ProductService);
 
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe((products) => {
+      this.displayProducts = products.slice(0, 2);
+    });
+  }
 }
