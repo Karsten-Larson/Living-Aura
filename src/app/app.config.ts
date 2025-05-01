999d67506418230385de6a8ea1f2f080909414b2
Karsten Larson <karsten.larson.1@gmail.com>
2025-05-01 16:42:29 -0500

    Firebase Config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,24 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCzRN8aPRSP6oNP2kR7sztL_ZqqyzfJTVk',
+  authDomain: 'living-aura.firebaseapp.com',
+  projectId: 'living-aura',
+  storageBucket: 'living-aura.firebasestorage.app',
+  messagingSenderId: '579401673753',
+  appId: '1:579401673753:web:f0364e6b1abe81d5798e88',
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
   ],
 };
