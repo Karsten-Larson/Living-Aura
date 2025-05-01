b0162189a9c51562273b63c71bfd7073ff8862db
Gavyn Nygren <gavyn.nygren@ndsu.edu>
2025-05-01 17:09:06 -0500

    Added checkout page

--- src/app/checkout/checkout/checkout.component.ts
+++ src/app/checkout/checkout/checkout.component.ts
@@ -1,16 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, FormsModule],
   standalone: true,
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css',
 })
 export class CheckoutComponent {
-  
   constructor() {}
-
-  
 }
